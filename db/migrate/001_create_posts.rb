bf1b2f5bbfaec8c90355062117499f935a6df95c
TomK32 <tomk32@tomk32.de>
2008-05-19 08:03:23 +0200

    added permalink

--- db/migrate/001_create_posts.rb
+++ db/migrate/001_create_posts.rb
@@ -6,6 +6,7 @@ class CreatePosts < ActiveRecord::Migration
       t.column :body, :text, :null => false
       t.column :published_at, :datetime, :null => false
       t.column :published, :boolean, :default => false
+      t.column :permalink, :string, :null => false      
       t.column :user_id, :integer, :null => false
 
       t.timestamps
