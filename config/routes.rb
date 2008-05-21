bc66f192e9a75d8c2adc71a0674eb13b8e1903fb
TomK32 <tomk32@tomk32.de>
2008-05-21 08:58:05 +0200

    routes for signup and registration

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,9 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  
+  map.login 'login', :controller => 'goldberg/auth', :action => 'login'
+  map.signup 'signup', :controller => 'goldberg/users', :action => 'self_register'
 
   # See how all your routes lay out with "rake routes"
 
