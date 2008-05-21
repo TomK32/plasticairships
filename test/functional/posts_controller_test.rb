5b917a80d5807af6fc750f6f1f7ef51ecdd10e86
TomK32 <tomk32@tomk32.de>
2008-05-21 14:41:11 +0200

    some test files, all empty...

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,8 +1,23 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_01_create_post_as_guest
+  end
+  
+  def test_02_create_post_as_member_and_moderator
+  end
+
+  # comment has no user_id
+  def test_03_comment_on_post_as_unregistered
+  end
+
+  # must update user attributes
+  # comment must have user_id
+  def test_04_comment_on_post_as_guest
+  end
+
+  # comment must have user_id
+  def test_05_comment_on_post_as_member    
   end
 end
