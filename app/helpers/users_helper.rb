67f9f747e6c0fa25adae11ffaf099a6980fbb02a
TomK32 <tomk32@tomk32.de>
2008-05-30 16:12:22 +0200

    added our own gravatar default image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
-  def gravatar(email, alt="", size=50, default=nil)
+  def gravatar(email, alt="", size=50)
     return '' unless email
-    url = "http://www.gravatar.com/avatar.php?size=#{size}&gravatar_id=#{Digest::MD5.hexdigest(email)}"
-    url << "&default=#{default}" if default
+    url = "http://www.gravatar.com/avatar.php?size=#{size}&gravatar_id=#{Digest::MD5.hexdigest(email)}&default=http://%s%s" % [request.host_with_port, image_path('gravatar.jpg')]
 
     image_tag url, :size => "#{size}x#{size}", :alt => alt
   end
