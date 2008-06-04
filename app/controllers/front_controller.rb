31124036023b74a3829a618beda62d24fda478e2
TomK32 <tomk32@tomk32.de>
2008-06-04 17:58:13 +0200

    inheritance won't work properly in dev mode

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -4,9 +4,14 @@ class FrontController < ApplicationController
 
   def index
     @posts = Post.find_published(2)
-    @post_comments = Post::Comment.recent.published.find(:all, :limit => 2, :include => :post)
-#    @site_comments = Site::Comment.recent.published.find(:all, :limit => 2, :include => :site)
-  @site_comments = []
+    @site_comments = Site::Comment.recent.published.find(:all, :limit => 5, :include => :site)
+    # FIXME rails' class reloading misses the modulized comments somehow
+    if RAILS_ENV == 'production'
+      @post_comments = Post::Comment.recent.published.find(:all, :limit => 5)
+    else
+      @post_comments = []
+    end
+
     @sites = Site.find_featured_published_with_screenshots(20)
     @content_page = Goldberg::ContentPage.find_by_name('home')
   end
