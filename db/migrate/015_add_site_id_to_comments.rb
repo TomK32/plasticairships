2809278eddd1cd71f7912bfbc9e2048c8880593e
TomK32 <tomk32@tomk32.de>
2008-06-05 23:37:11 +0200

    use acts_as_taggable_on_steroids instead of acts_as_taggable

--- db/migrate/015_add_site_id_to_comments.rb
+++ db/migrate/015_add_site_id_to_comments.rb
@@ -1,7 +1,7 @@
 class AddSiteIdToComments < ActiveRecord::Migration
   def self.up
     add_column :comments, :site_id, :integer
-    change_column :comments, :post_id, :integer
+    change_column :comments, :post_id, :integer # removing the :null => false
   end
 
   def self.down
