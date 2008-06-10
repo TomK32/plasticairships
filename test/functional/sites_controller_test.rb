3744d3a3debf2308d60d932f829d95ffff4bb7bd
TomK32 <tomk32@tomk32.de>
2008-06-10 19:17:25 +0200

    another test on errors sorting to-be-tested tag_list

--- test/functional/sites_controller_test.rb
+++ test/functional/sites_controller_test.rb
@@ -16,10 +16,11 @@ class SitesControllerTest < ActionController::TestCase
     assert_equal false, @controller.current_user
 
     # no success with this incomplete one
-    post :create, :site => {:title => 'my website'}
+    post :create
     assert_not_nil assigns['site']
     site = assigns['site']
     # check for the correct error messages
+    assert site.errors.on(:title)
     assert site.errors.on(:url)
     assert site.errors.on(:permalink)
     assert site.errors.on(:description)
@@ -37,12 +38,18 @@ class SitesControllerTest < ActionController::TestCase
     assert_equal false, @response.redirect?
     assert_not_nil assigns['site']
     site = assigns['site']
+    
+    # no errors of course
     assert site.errors.empty?
+
+    # site must be saved and not published as it's a guest user
     assert_equal sites_counter+1, Site.count
     assert_equal sites_counter, Site.published.count
     assert_equal false, site.new_record?
     assert_equal false, site.published?
-    assert_equal %w(tomk32 blog germany), site.tag_list
+    assert_equal %w(blog germany tomk32), site.tag_list.sort
+    
+    # it must be the same guest user, otherwise we spam the user table
     assert_equal guest_user, @controller.current_user
   end
 end
